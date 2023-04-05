3ca70e3384bdf6a2a0d620f1a2d3e2630cd8eb9a
yamaguchi <yamaguchi.takahiro.52c@gmail.com>
2023-04-06 01:40:49 +0900

    UpData

--- app/apps.py
+++ app/apps.py
@@ -1,11 +1,11 @@
 from django.apps import AppConfig
-from .make_project import function
+# import function
 
 
 class AppConfig(AppConfig):
     # 追加
-    list_route = function.make()
-    function.print_prj(list_route)
+    # list_route = function.make()
+    # function.print_prj(list_route)
     #
     default_auto_field = "django.db.models.BigAutoField"
     name = "app"
--- app/views.py
+++ app/views.py
@@ -1,7 +1,17 @@
 from django.views.generic import View
 from django.shortcuts import render
+from. import function
 
 
 class IndexView(View):
+    template_name = 'app/index.html'
+    
     def get(self, request, *args, **kwargs):
+        list_route = function.make()
+        function.print_prj(list_route)    
         return render(request, "app/index.html")
+
+
+# class IndexView(View):
+#     def get(self, request, *args, **kwargs):
+#         return render(request, "app/index.html")
